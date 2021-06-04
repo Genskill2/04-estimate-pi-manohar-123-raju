626c624c579130c2c9b77da27469d9cc8360d817
manohar-123-raju <85029964+manohar-123-raju@users.noreply.github.com>
2021-06-04 11:43:29 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -12,17 +12,15 @@ float frandom() {
   
 float mc_pi(int n){
    int a = 0;
-   float c;
    for( int i = 0 ; i < n; i++){
     float x = frandom();
     float y = frandom();
     float d = x*x+y*y-x-y+0.25;
-    if(d < 0){
+    if(d <= 0){
      a++;
     }
    }
-   c = a/n
-   float pi = 4*c;
+   float pi = 4*(float)a/n;
    return pi;
 } 
 
