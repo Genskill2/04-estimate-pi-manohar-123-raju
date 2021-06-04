5591b544cbcebd3d25e4ad7b97ec645afe25c8f7
manohar-123-raju <85029964+manohar-123-raju@users.noreply.github.com>
2021-06-04 10:58:05 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -10,19 +10,19 @@ float frandom() {
   float ret = (float)q/(float)RAND_MAX;
   return ret;
   }
- float mc_pi(int n){
-   int incircle = 0;
+float mc_pi(int n){
+  int incircle = 0;
 
-for( int i = 0 ; i < n; i++){
-float x = frandom();
-float y = frandom();
-float d = x*x+y*y;
-if(d <= 1){
-incircle++;
- }
-}
-float pi = 4*(float)incircle/n;
-return pi;
+  for( int i = 0 ; i < n; i++){
+   float x = frandom();
+   float y = frandom();
+   float d = x*x+y*y;
+   if(d <= 1){
+    incircle++;
+   }
+  }
+ float pi = 4*(float)incircle/n;
+ return pi;
 } 
 
 
